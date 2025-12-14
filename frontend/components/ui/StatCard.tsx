f0696812bf129c5160177342590112df3c4d4176
elidotco <eddiefestde@gmail.com>
2025-12-14 02:22:04 +0000

    changes stuck

--- frontend/components/ui/StatCard.tsx
+++ frontend/components/ui/StatCard.tsx
@@ -22,52 +22,94 @@ const StatCard = ({ name, value, prev, amount }: StatCardProps) => {
       </div>
       <p className="text-gray-500 text-sm">Last 7 days</p>
       <div className="flex pt-5">
-        {value.map((val) => (
-          <div
-            key={val.name}
-            className="flex items-center text-center justify-center "
-          >
-            <p className="text-3xl font-semibold text-center text-text-color">
-              ${val.value}
-            </p>
-            <div className="flex pl-2  items-center">
+        {value.map((val) =>
+          value.length > 1 ? (
+            <div
+              key={val.name}
+              className="flex items-center text-center justify-center "
+            >
               <p className="pr-1">{val.name}</p>
-              {val.analysis === "Up" ? (
-                <Icon
-                  icon="akar-icons:arrow-up"
-                  className="text-green-500"
-                  width="16"
-                  height="16"
-                />
-              ) : val.analysis === "Down" ? (
-                <Icon
-                  icon="akar-icons:arrow-down"
-                  className="text-red-500"
-                  width="16"
-                  height="16"
-                />
-              ) : null}
-              {val.percent !== undefined && (
-                <p
-                  className={`text-sm ml-1 ${
-                    val.analysis === "Up"
-                      ? "text-green-500"
-                      : val.analysis === "Down"
-                      ? "text-red-500"
-                      : "text-text-color"
-                  }`}
-                >
-                  {val.percent}%
-                </p>
-              )}
+              <p className="text-3xl font-semibold text-center text-text-color">
+                ${val.value}
+              </p>
+              <div className="flex pl-2  items-center">
+                {val.analysis === "Up" ? (
+                  <Icon
+                    icon="akar-icons:arrow-up"
+                    className="text-green-500"
+                    width="16"
+                    height="16"
+                  />
+                ) : val.analysis === "Down" ? (
+                  <Icon
+                    icon="akar-icons:arrow-down"
+                    className="text-red-500"
+                    width="16"
+                    height="16"
+                  />
+                ) : null}
+                {val.percent !== undefined && (
+                  <p
+                    className={`text-sm ml-1 ${
+                      val.analysis === "Up"
+                        ? "text-green-500"
+                        : val.analysis === "Down"
+                        ? "text-red-500"
+                        : "text-text-color"
+                    }`}
+                  >
+                    {val.percent}%
+                  </p>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          ) : (
+            <div
+              key={val.name}
+              className="flex items-center text-center justify-center "
+            >
+              <p className="text-3xl font-semibold text-center text-text-color">
+                ${val.value}
+              </p>
+              <div className="flex pl-2  items-center">
+                <p className="pr-1">{val.name}</p>
+                {val.analysis === "Up" ? (
+                  <Icon
+                    icon="akar-icons:arrow-up"
+                    className="text-green-500"
+                    width="16"
+                    height="16"
+                  />
+                ) : val.analysis === "Down" ? (
+                  <Icon
+                    icon="akar-icons:arrow-down"
+                    className="text-red-500"
+                    width="16"
+                    height="16"
+                  />
+                ) : null}
+                {val.percent !== undefined && (
+                  <p
+                    className={`text-sm ml-1 ${
+                      val.analysis === "Up"
+                        ? "text-green-500"
+                        : val.analysis === "Down"
+                        ? "text-red-500"
+                        : "text-text-color"
+                    }`}
+                  >
+                    {val.percent}%
+                  </p>
+                )}
+              </div>
+            </div>
+          )
+        )}
       </div>
       {prev && (
         <div className="flex items-center pt-2 ">
-          <p className="text-sm text-gray-500 ml-1">Previous 7days</p>{" "}
-          <span className="text-tertiary">(203)</span>
+          <p className="text-sm text-gray-500 ml-1">Previous 7 days</p>{" "}
+          <span className="text-tertiary">({amount})</span>
         </div>
       )}
     </div>
